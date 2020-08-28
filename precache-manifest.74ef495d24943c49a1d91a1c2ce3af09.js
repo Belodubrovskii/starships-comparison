10e0f31181f21ad057a62ea3614d1c0c43c81b6c
Belodubrovskii Daniil <dbelodubrovskij@yandex.ru>
2020-08-28 18:27:33 +0300

    Updates

---
+++ precache-manifest.74ef495d24943c49a1d91a1c2ce3af09.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11318e4decdc8896a35e1ff435840a0",
+    "url": "/starships-comparison/index.html"
+  },
+  {
+    "revision": "dd1c75411dcfc549720a",
+    "url": "/starships-comparison/static/css/main.7d0ba2dd.chunk.css"
+  },
+  {
+    "revision": "a0c2bd1a59a82552525f",
+    "url": "/starships-comparison/static/js/2.2d8403da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starships-comparison/static/js/2.2d8403da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1c75411dcfc549720a",
+    "url": "/starships-comparison/static/js/main.cb8d518a.chunk.js"
+  },
+  {
+    "revision": "cd2ac1f61ef9a7ccbba8",
+    "url": "/starships-comparison/static/js/runtime-main.6b3f870b.js"
+  },
+  {
+    "revision": "0bed5df3ee4932889773d4425ff5f77c",
+    "url": "/starships-comparison/static/media/10.0bed5df3.png"
+  },
+  {
+    "revision": "718fe2727e57f4d22bbe0c219b7712d6",
+    "url": "/starships-comparison/static/media/12.718fe272.png"
+  },
+  {
+    "revision": "cec3feddea43972a80e16b13bf37555a",
+    "url": "/starships-comparison/static/media/22.cec3fedd.png"
+  },
+  {
+    "revision": "af84c44c47e0fad6ee28072fb3662cdf",
+    "url": "/starships-comparison/static/media/Star_Wars_Yellow_Logo.af84c44c.png"
+  }
+]);
